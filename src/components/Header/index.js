a14a80fa229018971a9ed496fe01c1e33487aeae
itspanicky <nicky.li92@gmail.com>
2021-01-14 18:24:03 -0500

    feat: setup Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './index.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatar for logged in user */}
+        {/* Time Icon */}
+      </div>
+      <div className="header__search">
+        {/* Search icon */}
+        {/* Input */}
+      </div>
+      <div className="header__right">
+        {/* Help icon */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
