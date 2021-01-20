d54ca0a348b545650e70ac1f4e770a4fc0a0feaa
itspanicky <nicky.li92@gmail.com>
2021-01-19 20:03:49 -0500

    feat: setup chats

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import Header from "./components/Header"
-import Sidebar from "./components/Sidebar"
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Chat from './components/Chat'
 
 
 function App() {
   return (
-    <div className="app">
-      <h1>Slack Clone</h1>
-      <Header/>
-      <div className='app__body'>
-        <Sidebar />
-        {/* React Router */}
-      </div>
+    <div className='app'>
+      <Router>
+        <Header/>
+        <div className='app__body'>
+          <Sidebar />
+          <Switch>
+            <Route path='/room/:roomId' component={Chat} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
