3c10a3e62060fd0c634007c01b2cb1ad45c7572b
itspanicky <nicky.li92@gmail.com>
2021-01-21 18:23:34 -0500

    refactor: areas that require user from state

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -14,9 +14,11 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import db from '../../firebase';
+import { useStateValue } from '../../StateProvider';
 
 function Sidebar() {
   const [channels, setChannels] = useState([]);
+  const [{ user }] = useStateValue();
 
   useEffect(() => {
     // Run this code when the sidebar component loads
@@ -37,7 +39,7 @@ function Sidebar() {
           <h2>slack-clone workspace</h2>
           <h3>
             <FiberManualRecordIcon />
-            Nicky Li
+            {user?.displayName}
           </h3>
         </div>
         <CreateIcon />
