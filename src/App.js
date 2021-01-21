2e443d59ef3745f96b8f6756652aeecc1f574474
itspanicky <nicky.li92@gmail.com>
2021-01-21 18:22:56 -0500

    feat: implement google auth and store user with context api

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import './App.css';
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+import Login from './components/Login'
+import { useStateValue } from './StateProvider'
 
 
 function App() {
+  const [{ user }] = useStateValue();
+
   return (
     <div className='app'>
       <Router>
-        <Header/>
-        <div className='app__body'>
-          <Sidebar />
-          <Switch>
-            <Route path='/room/:roomId' component={Chat} />
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header/>
+            <div className='app__body'>
+              <Sidebar />
+              <Switch>
+                <Route path='/room/:roomId' component={Chat} />
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+export const initialState = {
+  user: null,
+}
+
+export const actionTypes = {
+  SET_USER: 'SET_USER',
+}
+
+// reducer listens for an action
+const reducer = (state, action) => {
+  console.log(action)
+
+  switch(action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer;
