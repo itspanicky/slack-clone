ab58c87bacbe22c35ae04890c0dd14b153d28c30
itspanicky <nicky.li92@gmail.com>
2021-01-22 20:12:00 -0500

    feat: setup firebase hosting

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,14 @@
 import firebase from 'firebase';
-import { firebaseConfig} from './firebase-config'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPmz2XBX9ljHvWOFm-CVPnlWK60JXW3o0",
+  authDomain: "slack-clone-d6702.firebaseapp.com",
+  projectId: "slack-clone-d6702",
+  storageBucket: "slack-clone-d6702.appspot.com",
+  messagingSenderId: "234895882227",
+  appId: "1:234895882227:web:c450fe01b56de149604486",
+  measurementId: "G-6H5JLJ4ETS"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
